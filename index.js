5c5304d955389d864048f177120a721449b11044
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-11 05:31:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ session.consult(":- use_module(library(js)).");
 const user_avatar = 'https://image.flaticon.com/icons/svg/1400/1400241.svg'
 const subway_avatar = 'https://image.flaticon.com/icons/svg/1995/1995600.svg'
 const messages = {
-  greetings: `Hello Ben !
-  <br/>
+  greetings: `
   Welcome To Subway!
   <br/>
   <br/>
