403f5cdac5066973c004cdee1e1aa6e225f75f67
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-10 22:02:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ function buttonClicked(fact) {
             if (pl.type.is_substitution(answer)) {
               // Get the value of the food
               var output = answer.lookup("X");
-              console.log(output);
+              console.log(output[0].id);
             }
           };
         }
