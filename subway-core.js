371277fc0c38db059a4de2a2858d4d86aa3d9618
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-12 03:48:09 +0800

    k

--- subway-core.js
+++ subway-core.js
@@ -56,23 +56,57 @@ non_vegan_topups(L1), vegan_topups(L2), append(L1, L2, X))), X).
 get_sides(X) :- sides(X).
 
 % Declare predicates for getting the menu item list based on current arguments and creates a menu for GUI
-options(meals) :- get_meals(L), createDOMV1(L).
-options(sauces) :- get_sauces(L), createDOMV2(L).
-options(breads) :- get_breads(L), createDOMV1(L).
-options(meats) :- get_meats(L), createDOMV2(L).
-options(veggies) :- get_veggies(L), createDOMV1(L).
-options(topups) :- get_topups(L), createDOMV2(L).
-options(sides) :- get_sides(L), createDOMV1(L).
+
+options(meals) :-
+get_meals(L), 
+createDOMV1(L).
+
+options(sauces) :- 
+get_sauces(L), 
+createDOMV2(L).
+
+options(breads) :- 
+get_breads(L), 
+createDOMV1(L).
+
+options(meats) :- 
+get_meats(L), 
+createDOMV2(L).
+
+options(veggies) :- 
+get_veggies(L), 
+createDOMV1(L).
+
+options(topups) :- 
+get_topups(L), 
+createDOMV2(L).
+
+options(sides) :- 
+get_sides(L), 
+createDOMV1(L).
 
 % Declare predicates for asserting facts if given input is not already selected
 % only will assert if X is not in the selected list e.g check_selection(X, meals) is false
-selected(X,meals) :- \\+check_selection(X, meals) -> asserta(selected_meals(X)).
-selected(X,breads) :- \\+check_selection(X, breads) -> asserta(selected_breads(X)).
-selected(X,meats) :- \\+check_selection(X, meats) ->asserta(selected_meats(X)).
-selected(X,veggies) :- \\+check_selection(X, veggies) ->asserta(selected_veggies(X)).
-selected(X,sauces) :- \\+check_selection(X, sauces) ->asserta(selected_sauces(X)).
-selected(X,topups) :- \\+check_selection(X, topups) ->asserta(selected_topups(X)).
-selected(X,sides) :- \\+check_selection(X, sides) ->asserta(selected_sides(X)).
+selected(X,meals) :-
+ \\+check_selection(X, meals) -> asserta(selected_meals(X)).
+
+selected(X,breads) :- 
+\\+check_selection(X, breads) -> asserta(selected_breads(X)).
+
+selected(X,meats) :- 
+\\+check_selection(X, meats) ->asserta(selected_meats(X)).
+
+selected(X,veggies) :- 
+\\+check_selection(X, veggies) ->asserta(selected_veggies(X)).
+
+selected(X,sauces) :- 
+\\+check_selection(X, sauces) ->asserta(selected_sauces(X)).
+
+selected(X,topups) :-
+ \\+check_selection(X, topups) ->asserta(selected_topups(X)).
+
+selected(X,sides) :-
+ \\+check_selection(X, sides) ->asserta(selected_sides(X)).
 
 % Declare predicates for checking if X is already in selected list
 check_selection(X, breads):- 
@@ -100,25 +134,32 @@ selected_sides(L), member(X,L),!.
 % e.g findall(X, pred(X), List) - Finds possible values for predicate and displays them on the GUI
 
 show_meals(Meals) :- 
-findall(X, selected_meals(X), Meals), displayOrder('Meal:',Meals).
+findall(X, selected_meals(X), Meals),
+ displayOrder('Meal:',Meals).
 
 show_breads(Breads) :-
-findall(X, selected_breads(X), Breads), displayOrder('Breads: ',Breads).
+findall(X, selected_breads(X), Breads), 
+displayOrder('Breads: ',Breads).
 
 show_meats(Meats) :- 
-findall(X, selected_meats(X), Meats), displayOrder('Meats: ',Meats).
+findall(X, selected_meats(X), Meats), 
+displayOrder('Meats: ',Meats).
 
 show_veggies(Veggies) :- 
-findall(X, selected_veggies(X), Veggies), displayOrder('Veggies: ',Veggies).
+findall(X, selected_veggies(X), Veggies), 
+displayOrder('Veggies: ',Veggies).
 
 show_sauces(Sauces) :- 
-findall(X, selected_sauces(X), Sauces), displayOrder('Sauces: ',Sauces).
+findall(X, selected_sauces(X), Sauces), 
+displayOrder('Sauces: ',Sauces).
 
 show_topups(Topups) :- 
-findall(X, selected_topups(X), Topups), displayOrder('Topups: ',Topups).
+findall(X, selected_topups(X), Topups), 
+displayOrder('Topups: ',Topups).
 
 show_sides(Sides) :- 
-findall(X, selected_sides(X), Sides), displayOrder('Sides: ',Sides).
+findall(X, selected_sides(X), Sides), 
+displayOrder('Sides: ',Sides).
 
 % Declare predicate for displaying all the selected ingredients for the final order on the GUI if input X is 1
 displaySelections(X) :- 
