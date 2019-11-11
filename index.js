149d73272586d799a18b51c69ded08c4aaf4d14f
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-11 20:26:21 +0800

    df

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ function buttonClicked(fact) {
 
 // ---- Print Messages
 updateDialogueBox('staff', messages.greetings)
-console.log('1')
+console.log('21')
 $("#btn-group").empty()
 session.query("options(meals).")
 session.answer()
--- subway-prolog.js
+++ subway-prolog.js
@@ -113,8 +113,8 @@ show_sides(sides) :- findall(X, chosen_sides(X), Sides), displayOrder(Sides).
 
 display(X) :- 
 (X==1) ->
-show_meals(meals).
-
+show_meals(meals),
+show_breads(breads).
  %show_breads(breads), 
  %show_meats(meats),
  %show_veggies(veggies),
