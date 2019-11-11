b389fcd8c6a596d77947e91bc12e7706c3d9213a
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-11 08:26:09 +0800

    Update subway-prolog.js

--- subway-prolog.js
+++ subway-prolog.js
@@ -151,7 +151,7 @@ create(a, A),
 createMenuItems(H) :-                                    
 create(a, A),                                         
     html(A, H),
-create(br, BR), 'I would like '                                        
+create(br, BR),                                      
     get_by_id('subway-contents', Parent),
     append_child(Parent, BR),
     append_child(Parent, A).
