171ce68cc91fb264d4341b3e8212f7de8e2144b3
rukidevilwalk <zcphoivos@hotmail.com>
2019-11-11 21:22:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ function buttonClicked(fact) {
 }
 
 updateDialogueBox('staff', messages.greetings)
-console.log('Version' + ' 2')
+console.log('Version' + ' 1')
 $("#btn-group").empty()
 session.query("options(meals).")
 session.answer()
